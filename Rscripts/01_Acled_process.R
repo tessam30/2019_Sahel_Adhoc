ca34a4df497d396ed21ffce406f189eb245fa974
Tessam <tim.essam@gmail.com>
2019-08-08 15:44:54 -0400

    added new plots of coup data and acled

--- Rscripts/01_Acled_process.R
+++ Rscripts/01_Acled_process.R
@@ -30,11 +30,67 @@ conflict <-
 write_csv(conflict, file.path(dataout, "ACLED_conflict_Africa.csv"))
 write_csv(conflict %>% filter(Sahel_flag == 1), file.path(dataout, "ACLED_conflict_Sahel.csv"))
 
-conflict %>% 
-  group_by(year, country, event_type) %>% 
+conflict2 <- 
+  conflict %>% 
+  filter(Sahel_flag == 1) %>% 
+  group_by(year, country) %>% 
   summarise(count = n()) %>% 
-  spread(year, count)
+  mutate(color_flag = ifelse(count >100, grey10K, grey80K)) %>% 
+  ungroup()
 
+# Heatmap of acled events
+conflict2 %>% 
+  ggplot(aes(x = year, y = country, fill = count)) +
+  geom_tile(colour = "white", size = 0.25) +
+  scale_fill_viridis_c(option = "A", direction = -1, alpha = 1,
+                       name = "Conflict events")  +
+  scale_x_continuous(breaks = seq(1997, 2020, 5)) +
+  theme_xygrid() +
+  theme(legend.position = "top",
+        panel.grid.major.x = element_blank(),
+        plot.margin = margin(0, -5, 0, 0),
+        panel.grid.major.y = element_blank(),
+        axis.ticks.x = element_line(),
+        # Moving the country titles closer to graph, not sure this is the best approach
+        axis.text.y.left = element_text(margin = margin(r = -20)),
+        text = element_text(size = 14,  family = "Lato"), 
+        plot.title = element_text(size = 14, family = "Lato", colour = grey60K),
+        plot.caption = element_text(size = 10, family = "Lato", colour = grey60K,
+                                    hjust = 0.9)) +
+  coord_fixed(ratio = 1) +
+  labs(x = "", y = "",
+       title = "Conflict events in the Sahel",
+       caption = "Armed Conflict and Event Location Data (ACLED)") +
+  geom_text(aes(label = count, color = color_flag), family = "Lato", size = 3) +
+  scale_color_manual(values = c(grey50K, "white")) +
+  guides(colour = FALSE, fill = FALSE)
+
+ggsave(file.path(imagepath, "Sahel_Acled_events_1997_2019.png"),
+       plot = last_plot(),
+       width = 11,
+       height = 6.25,
+       units = c("in"),
+       device = "png",
+       dpi = "retina")
+
+# OR what does a timeseries plot look like?
+
+conflict2 %>% 
+  ggplot(aes(x = year, y = count, group = country, color = country)) +
+  geom_line() + 
+  facet_wrap(~country, nrow = 5, scale = "free_y") + 
+  theme_line +
+  scale_color_manual(values = country_colors) +
+  scale_x_continuous(breaks = seq(1997, 2022, 5)) +
+  labs(x = "", y = "", caption = "Armed Conflict and Event Location Data (ACLED)",
+       title = "Armed Conflict Events Across the Sahel")
+
+ggsave(filename = file.path(imagepath, "ACLED_Sahel_Time_Series.png"), 
+       plot = last_plot(),
+       height = 8, 
+       width = 11.5, 
+       units = "in",
+       device = "png")  
 
 
   
--- Rscripts/Sahel_WDI_plots.R
+++ Rscripts/Sahel_WDI_plots.R
@@ -90,5 +90,71 @@ for (i in 1:length(plot_list)) {
          units = "in",
          device = "png")
 }
+
+
+# Read in coup data -------------------------------------------------------
+# From: https://www.jonathanmpowell.com/coup-detat-dataset.html
+
+# Create an blank row for 1965 for each country
+pre_coup <- tibble::tribble(
+                    ~country, ~ccode, ~year, ~month, ~day, ~coup,
+              "Burkina Faso",    439,  1965,      1,    1,    NA,
+                      "Mali",    432,  1965,      1,    1,    NA,
+                     "Niger",    436,  1965,      1,    1,    NA,
+                      "Chad",    483,  1965,      1,    1,    NA,
+                "Mauritania",    435,  1965,      1,    1,    NA,
+              "Burkina Faso",    439,  2020,      1,    1,    NA,
+                      "Mali",    432,  2020,      1,    1,    NA,
+                     "Niger",    436,  2020,      1,    1,    NA,
+                      "Chad",    483,  2020,      1,    1,    NA,
+                "Mauritania",    435,  2020,      1,    1,    NA,
+              "Burkina Faso",    439,  2016,     10,    8,     1
+              )
+
+
+coup_url <- c("http://www.uky.edu/~clthyn2/coup_data/powell_thyne_coups_final.txt")
+coups <- read_tsv(url(coup_url)) %>% 
+  filter(country %in% c("Burkina Faso", "Chad", "Mali", "Mauritania", "Niger")) %>%
+  bind_rows(., pre_coup) %>% 
+  complete(country, year = full_seq(year, 1)) %>% 
+  mutate(coup_success = ifelse(is.na(coup), 0, coup),
+         coup_success = ifelse(country == "Niger" & year %in% c(1976, 1983), 0, coup_success))
+
+coups %>% 
+  ggplot(aes(x = year, y = country, fill = factor(coup_success))) +
+  geom_tile(colour = "white", size = 0.25) +
+  scale_fill_manual(values = c(grey10K, "#f0c6c3","#d46780"),
+                    labels = c("No Coup Attemp", "Unsuccessful Coup", "Successful Coup"),
+                    name = "") +
+  scale_x_continuous(breaks = seq(1965, 2015, 5), limits = c(1964, 2020)) +
+  theme_xygrid() +
+  theme(legend.position = "top",
+        panel.grid.major.x = element_blank(),
+        plot.margin = margin(0, -5, 0, 0),
+        panel.grid.major.y = element_blank(),
+        axis.ticks.x = element_line(),
+        # Moving the country titles closer to graph, not sure this is the best approach
+        axis.text.y.left = element_text(margin = margin(r = -20)),
+        text = element_text(size = 14,  family = "Lato"), 
+        plot.title = element_text(size = 14, family = "Lato", colour = grey60K),
+        plot.caption = element_text(size = 10, family = "Lato", colour = grey60K,
+                                    hjust = 0.9)) +
+  coord_fixed(ratio = 1) +
+  labs(x = "", y = "",
+       title = "Political instability in the Sahel",
+       caption = "Source: https://www.jonathanmpowell.com/coup-detat-dataset.html") 
+
+
+  ggsave(file.path(imagepath, "Sahel_coup_history.png"),
+         plot = last_plot(),
+         width = 11,
+         height = 6.25,
+         units = c("in"),
+         device = "png",
+         dpi = "retina")
+
+  
+
+
   
 
